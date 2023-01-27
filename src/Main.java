a5c1478367ce4e8bdd2cd655be3b8bf8523d156f
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-01-27 21:42:05 +0530

    Evaluates Length of line

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Here we have to draw line by using cartesian method \n" + "To draw line we need 2 referance points \n" + "By taking co-ordinates of point");
+        System.out.println("Enter X co-ordinate of point 1 : ");
+        int x1 = sc.nextInt();
+        System.out.println("Enter Y co-ordinate of point 1 : ");
+        int y1 = sc.nextInt();
+        System.out.println("Enter X co-ordinate of point 2 : ");
+        int x2 = sc.nextInt();
+        System.out.println("Enter Y co-ordinate of point 2 : ");
+        int y2 = sc.nextInt();
+        double length = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+        System.out.println("length of line : " +length);
     }
 }
