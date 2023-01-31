3c6bbb7923b72d12f18bcc3d95cd35be689bec91
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-01-31 16:11:14 +0530

    checks the length of lines & tells which line is greater

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,28 @@ public class Main {
 
     public static void main(String[] args)
     {
-        Main m = new Main();
+
         System.out.println("""
                 Here we have to draw line by using cartesian method\s
                 In cartesian method to draw line we need 2 reference points\s
                 
                 please Enter the co-ordinates of points\s
                  """);
-       double length1 = m.line();
-        System.out.println("Enter the Co-Ordinates for 2nd line");
-        double length2 = m.line();
+       double length1 = line();
+        System.out.println("\nEnter the Co-Ordinates for 2nd line\n");
+        double length2 = line();
         System.out.println(Objects.equals(length1, length2));
+        if(length1 == length2){
+            System.out.println("Lengths of both the lines are Equal");
+        } else if (length1 > length2) {
+            System.out.println("Length of line 1 is greater than line 2");
+
+        } else {
+            System.out.println("Length of line 2 is greater than line 1");
+        }
+
     }
-    public double line(){
+    public static double line(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter X co-ordinate of point 1 : ");
         int x1 = sc.nextInt();
