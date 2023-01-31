78f04343fd4507a1b8dbab94ce0b9e6f51bdae3d
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-01-31 13:57:20 +0530

    Checks Equality of lines using java equals method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,24 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args)
     {
+        Main m = new Main();
+        System.out.println("""
+                Here we have to draw line by using cartesian method\s
+                In cartesian method to draw line we need 2 reference points\s
+                
+                please Enter the co-ordinates of points\s
+                 """);
+       double length1 = m.line();
+        System.out.println("Enter the Co-Ordinates for 2nd line");
+        double length2 = m.line();
+        System.out.println(Objects.equals(length1, length2));
+    }
+    public double line(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Here we have to draw line by using cartesian method \n" + "To draw line we need 2 referance points \n" + "By taking co-ordinates of point");
         System.out.println("Enter X co-ordinate of point 1 : ");
         int x1 = sc.nextInt();
         System.out.println("Enter Y co-ordinate of point 1 : ");
@@ -15,5 +29,8 @@ public class Main {
         int y2 = sc.nextInt();
         double length = Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
         System.out.println("length of line : " +length);
+        return length;
+
     }
+
 }
